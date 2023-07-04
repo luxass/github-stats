7ebee34e580e98e57064910075bae8f9c972e5f5
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-07-04 17:58:32 +0200

    more nuxt

--- app.vue
+++ app.vue
@@ -1,5 +1,67 @@
+<script setup>
+import "@unocss/reset/tailwind.css";
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
+
+<style>
+html {
+    @apply antialiased touch-manipulation scroll-smooth text-base;
+    text-rendering: optimizeLegibility;
+  }
+
+  body {
+    @apply text-neutral-800 bg-neutral-100 dark:(text-neutral-300 bg-neutral-900) h-screen w-screen font-sans;
+  }
+
+  .prose {
+    @apply [&>:not(:last-child)]:mb-6;
+
+    & p {
+      @apply line-height-relaxed;
+    }
+
+    & h1,
+    & h2,
+    & h3,
+    & h4,
+    & h5,
+    & h6 {
+      @apply text-neutral-800 dark:text-neutral-100 font-semibold my-8;
+    }
+
+    & h1 {
+      @apply text-2xl;
+    }
+
+    & h2 {
+      @apply text-xl;
+    }
+
+    & h3 {
+      @apply text-lg;
+    }
+
+    & h4 {
+      @apply text-base;
+    }
+
+    & h5 {
+      @apply text-sm;
+    }
+
+    & h6 {
+      @apply text-xs;
+    }
+
+    & strong {
+      @apply font-medium text-neutral-800 dark:text-neutral-100;
+    }
+
+    & img {
+      @apply mx-auto rounded my-6;
+    }
+  }
+</style>
