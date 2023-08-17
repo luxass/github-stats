f5a00170d1d2465cf27988184f1b6f7339d404f7
luxass <lucasnrgaard@gmail.com>
2023-08-17 17:30:48 +0200

    added analytics and bumped deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
   modules: ["@unocss/nuxt", "@unocss/nuxt"],
   devtools: {
     enabled: true
-  }
+  },
+  plugins: [{ src: "~/plugins/vercel-analytics.ts", mode: "client" }],
+  css: [
+    "@unocss/reset/tailwind.css",
+  ],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
 });
